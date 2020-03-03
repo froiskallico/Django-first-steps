ec905e866057aaaa32acd03b8dd39e7824b8d3de
KallicoFrois <froiskallico@gmail.com>
2020-03-03 14:56:49 -0300

    starting with automated tests

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'h%ujawy)@!_sw_#z&m(e-r42mldzdmeej@#$djp-rt^pv*mn9+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1'
+]
 
 
 # Application definition
